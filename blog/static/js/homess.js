3e049118a5e3c10ee32bcb7daf3a60c1ee7d99b9
pramodcyber <107044202+pramodcyber@users.noreply.github.com>
2022-06-21 09:35:55 +0530

    Add files via upload
    df

---
+++ blog/static/js/homess.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
---
+++ blog/static/js/swippers.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
